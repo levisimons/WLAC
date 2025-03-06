46bda61da08aa291893f24ba0cd5461e24ee8594
Ariel Levi Simons <levisimons@gmail.com>
2025-03-05 17:42:56 -0800

    Update Marine_SDM.R

--- Marine_SDM.R
+++ Marine_SDM.R
@@ -1,5 +1,6 @@
 rm(list=ls())
 require(data.table)
+require(sf)
 
 #Set working directory
 wd <- "" #Path to file on your own machine
@@ -8,3 +9,6 @@ setwd(wd)
 #Intake species occurrence data from GBIF
 #Available from querying GBIF: https://doi.org/10.15468/dl.j46jh7
 input_species_data <- fread(input="macrocystis_pyrifera.csv",sep="\t")
+
+#Convert species data to a spatial points object
+input_species_points <- st_as_sf(input_species_data, coords = c("decimalLongitude","decimalLatitude"),  crs = 4326) 
