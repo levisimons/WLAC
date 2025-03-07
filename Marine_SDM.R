1ea021e01c4003602aa17c724ea120646205758c
Ariel Levi Simons <levisimons@gmail.com>
2025-03-07 15:54:28 -0800

    Update Marine_SDM.R

--- Marine_SDM.R
+++ Marine_SDM.R
@@ -8,6 +8,7 @@ setwd(wd)
 
 #Intake species occurrence data from GBIF
 #Available from querying GBIF: https://doi.org/10.15468/dl.j46jh7
+#Copy of sample file here: https://github.com/levisimons/WLAC/blob/main/macrocystis_pyrifera.csv
 input_species_data <- fread(input="macrocystis_pyrifera.csv",sep="\t")
 
 #Convert species data to a spatial points object
