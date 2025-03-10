3b796f880b55f83d7f9ecdc7136d715ebd980dcb
Ariel Levi Simons <levisimons@gmail.com>
2025-03-10 16:59:50 -0700

    Update Marine_SDM.R

--- Marine_SDM.R
+++ Marine_SDM.R
@@ -16,3 +16,7 @@ input_species_points <- st_as_sf(input_species_data, coords = c("decimalLongitud
 #Source: https://earthworks.stanford.edu/catalog/stanford-sg211gq3741 
 #Relevant shapefile data is initially in the directory sg211gq3741/data_EPSG_4326 , move it to the same directory as the script.
 california_coast <- st_read("CA_cst3nm.shp")
+
+#Crop the California coastal boundary to a bounding box covering the Southern California Bight
+#Northwestern point of study area is Point Conception (34.4486° N, 120.4716° W), southeastern point is Tijuana Beach Promenade (32.5340° N, 117.1235° W)
+scb_boundaries <-  st_crop(california_coast,st_bbox(c(xmin=-120.4716,xmax=-117.1235,ymin=32.5340,ymax=34.4486)))
