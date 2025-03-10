ebf4bcd025c2d9cece50e1ff0d4a250b56ff6e7f
Ariel Levi Simons <levisimons@gmail.com>
2025-03-10 16:31:44 -0700

    Update Marine_SDM.R

--- Marine_SDM.R
+++ Marine_SDM.R
@@ -3,13 +3,16 @@ require(data.table)
 require(sf)
 
 #Set working directory
-wd <- "" #Path to file on your own machine
+wd <- ""
 setwd(wd)
 
 #Intake species occurrence data from GBIF
-#Available from querying GBIF: https://doi.org/10.15468/dl.j46jh7
-#Copy of sample file here: https://github.com/levisimons/WLAC/blob/main/macrocystis_pyrifera.csv
 input_species_data <- fread(input="macrocystis_pyrifera.csv",sep="\t")
 
 #Convert species data to a spatial points object
 input_species_points <- st_as_sf(input_species_data, coords = c("decimalLongitude","decimalLatitude"),  crs = 4326) 
+
+#Read in 3-Nautical Mile Coastal Boundary, California, 2001
+#Source: https://earthworks.stanford.edu/catalog/stanford-sg211gq3741 
+#Relevant shapefile data is initially in the directory sg211gq3741/data_EPSG_4326 , move it to the same directory as the script.
+california_coast <- st_read("CA_cst3nm.shp")
