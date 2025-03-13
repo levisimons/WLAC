2068fdd96ff5075121014c56a65bbe8e7f5a4129
Ariel Levi Simons <levisimons@gmail.com>
2025-03-12 18:13:23 -0700

    Update Marine_SDM.R

--- Marine_SDM.R
+++ Marine_SDM.R
@@ -133,4 +133,13 @@ sensitivity <- rf1$confusion[[1]] / (rf1$confusion[[1]]+rf1$confusion[[2]])
 specificity <- rf1$confusion[[4]] / (rf1$confusion[[4]]+rf1$confusion[[3]])
 TSS <- sensitivity+specificity-1
 #Store TSS results
-accuracy_list[i] <- TSS
+accuracy_list <- TSS
+
+#Loop through each environmental variable and store the partial response outputs in a temporary data frame.
+for(scb_layer in scb_layers){
+  tmp <- as.data.frame(partialPlot(rf1,subset_extracted[,!(colnames(subset_extracted) %in% "presence")],x.var=c(scb_layer),plot=F))
+  tmp$y <- exp(tmp$y) / (1+exp(tmp$y))
+  colnames(tmp) <- c(scb_layer,"Detection_Probability")
+  partial_plot_list[[j]] <- tmp
+  j <- j+1
+}
