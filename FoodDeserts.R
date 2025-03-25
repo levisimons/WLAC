e06c6f8500cc865c69fa08604362886580136ece
Ariel Levi Simons <levisimons@gmail.com>
2025-03-24 17:37:13 -0700

    Update FoodDeserts.R

--- FoodDeserts.R
+++ FoodDeserts.R
@@ -13,3 +13,14 @@ CommunityGardens <- fread(input="CommunityGardens.csv",sep=",")
 CommunityGardens <- st_as_sf(CommunityGardens,coords=c("Longitude","Latitude"),crs = st_crs(4326))
 #Transform the coordinates to a CRS of 2229 to match the map layers
 CommunityGardens <- st_transform(CommunityGardens,crs=st_crs(2229))
+
+#Read in food deserts map shapefiles
+Food_Deserts_Input <- st_read("Food_Deserts.shp")
+
+selected_variables <- c("PovertyRat","LA1and10")
+i=1
+selected_layer <- c()
+for(selected_variable in selected_variables){
+  selected_layer[[i]] <- st_rasterize(Food_Deserts_Input %>% dplyr::select(!!selected_variable, geometry))
+  i=i+1
+}
